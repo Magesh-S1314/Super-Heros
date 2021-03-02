bd8d2e25d94856e0f117bb9d0f74bc0c82c81a12
Magesh <magesh.s@doodleblue.com>
2021-03-02 08:16:03 +0530

    Initial Commit

---
+++ Super Heros/Super_HerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_HerosApp.swift
+//  Super Heros
+//
+//  Created by magesh on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Super_HerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
